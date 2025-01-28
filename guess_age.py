6141cafbde52827489dbca6155cc3fce5e2884e0
Suraj Yadav <surajyadav200701@gmail.com>
2025-01-28 19:43:59 +0530

    Learnt about Type COnversion

---
+++ guess_age.py
@@ -0,0 +1,13 @@
+print('Hello, World!')
+
+print('What is Your name ?')
+
+myname = input()
+
+print(myname + ' , what a Lovely name!')
+
+print(myname + ' , Tell me your age !')
+
+age = input()
+
+print('You will be ' + str(int(age) + 1) + ' in a year.')
