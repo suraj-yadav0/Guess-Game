b5a7cea7f629f36a5a1eba8fc6357ff2db80cbff
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-03 21:32:46 +0530

    learnt about range

--- second.py
+++ second.py
@@ -1,12 +1,5 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+x = range(2,45)
 
-a.append(9)
-a.append("Suresh")
-
-
-for i in a:
-   try:
-    if i < 5:
-        print(i)
-   except(ValueError, TypeError):
-       pass
+for elem in x:
+    if elem % 7 == 0:
+        print(elem)
