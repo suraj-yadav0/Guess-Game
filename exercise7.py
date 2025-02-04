ac4c581ebea4c82e246eedbe0cd014fd839a0185
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-04 13:02:42 +0530

    Created a List through the First List

--- exercise7.py
+++ exercise7.py
@@ -1,9 +1,21 @@
-year_of_birth = [1990, 1991, 1990, 1990, 1992, 1991]
+# year_of_birth = [1990, 1991, 1990, 1990, 1992, 1991]
 
-age = []
+# age = []
 
-for year in year_of_birth:
-    age.append(2025 - year)
+# for year in year_of_birth:
+#     age.append(2025 - year)
     
     
-print(age)
+# print(age)
+
+
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+b = []
+
+for i in a:
+    if i % 2 == 0:
+        b.append(i)
+        
+print(b)
