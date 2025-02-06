3c6abd42f528344f70fa92c93d6bbeb0a8e3dee0
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-06 13:18:50 +0530

    Learnt how to write function to take functions

---
+++ exercise11.py
@@ -0,0 +1,12 @@
+def get_int(help_text):
+    return int(input(help_text))
+
+
+
+
+age = get_int("How old are you? ")
+
+roll_no =  get_int("What is your roll number? ")
+
+
+print(f"Your age is {age} and your roll number is {roll_no}")
