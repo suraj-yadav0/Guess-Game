11501f9381375a8200dbd213b07f3bd9d18c6a3a
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-07 19:30:21 +0530

    created the random passoword genrator

--- password_genrator.py
+++ password_genrator.py
@@ -1,7 +1,11 @@
 import random
+import string
 
 
-list = random.sample(range(1, 100), 10)
+list = random.sample(range(10000), 10)
 
 
-print(random.sample(list, 1))
+password = random.choice(string.ascii_lowercase) + random.choice(string.ascii_uppercase) + str(random.choice(list)) + random.choice(string.punctuation)
+
+
+print(password)
