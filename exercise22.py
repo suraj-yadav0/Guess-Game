456ceb1625c9020bea679334d793cad6611f99f9
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-10 17:37:46 +0530

    implemented the Dictionary Data Structures

--- exercise22.py
+++ exercise22.py
@@ -1,6 +1,22 @@
-with open('exercise21.txt','r') as file:
-    all_text = file.read()
+# with open('exercise21.txt','r') as file:
+#     all_text = file.read()
     
     
     
-print(all_text)
+# print(all_text)
+
+
+
+dict = {'Alisha': 100 , 'Amit': 200 , 'Rahul': 300 , 'Rohit': 400 , 'Raj': 500}
+
+
+
+alishs = dict['Alisha']
+
+# print(alishs)
+
+
+dict['Alisha'] = 500
+
+for pair in dict:
+    print(pair, dict[pair])
