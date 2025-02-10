cc34b004902297e6e6ba83db1ccb905ace16b8a9
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-10 12:15:32 +0530

    Suucefully scrapped wiki and wrote to a file

--- exercise21.py
+++ exercise21.py
@@ -1,4 +1,19 @@
-with open ('exercise21.txt', 'w') as open_file:
-    open_file.write('Hello, World!')
-    
-    
+from bs4 import BeautifulSoup
+
+import requests
+
+url = 'https://en.wikipedia.org/wiki/Wiki'
+
+
+soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+
+
+# print(soup.find('div'),{'id':'mw-content-text'})
+
+text = ' '.join(p.get_text().strip() for p in soup.find_all('p') if p.get_text().strip())
+
+
+
+
+with open('exercise21.txt','w',encoding='utf-8') as open_file:
+    open_file.write(text)
