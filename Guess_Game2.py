326a302bbfce914d376b25493e6d2d5b72854163
Suraj Yadav <surajyadav200701@gmail.com>
2025-02-11 12:32:58 +0530

    Also implemented a counter to maintain the counts

--- Guess_Game2.py
+++ Guess_Game2.py
@@ -4,14 +4,17 @@
 def guess():
     low = 1
     high = 100
+    count = 1
     
     while(low <= high):
         mid = (low + high)//2
         target = input(f"Is {mid} your number? (yes/no): ")
         if target =="yes":
-            print("Hooray! I guessed the number.")
+            print("Hooray! I guessed the number in ", count, "attempts")
             return
         elif target == "no":
+            count += 1
+            
             target = input(f"Is the number less than {mid}? (yes/no): ")
             if target == "yes":
                 high = mid - 1
